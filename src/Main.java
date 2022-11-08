0d92fabbcb7737bbb34f4f70ef76573471df4a1e
Khashem <hashemehraz@gmail.com>
2022-11-08 16:33:18 +0600

    commit for local repo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Я закоммитил новый коммит");
+        System.out.println("Это строка только для локального репозитория");
+
     }
 }
